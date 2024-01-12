cf2b35261e80c3226ffc78bddc2c6af3fd0a2c81
LucaCaligola <luca.caligola@gmail.com>
2024-01-12 17:19:36 +0100

    created a new index.php and a new variable

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+$variabile = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut nisi saepe sunt vero dolore nihil quisquam consequuntur cupiditate. Ipsam nisi, aut atque vel obcaecati nostrum iure laboriosam numquam autem. Eum?"
+
+
+?>
+
+
+<h1><?php echo $variabile ?></h1>
+
+<h2> Lunghezza paragrafo <?php echo strlen($variabile)?></h2>
+
