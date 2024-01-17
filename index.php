7418cd15709e0dcc2c3c1001a9193d8a646c408d
LucaCaligola <luca.caligola@gmail.com>
2024-01-17 17:10:28 +0100

    i added the GET and created the method to censure the badword

--- index.php
+++ index.php
@@ -1,12 +1,33 @@
 <?php
 
-$variabile = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut nisi saepe sunt vero dolore nihil quisquam consequuntur cupiditate. Ipsam nisi, aut atque vel obcaecati nostrum iure laboriosam numquam autem. Eum?"
+$variabile = "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut nisi saepe sunt vero dolore nihil quisquam consequuntur cupiditate. Ipsam nisi, aut atque vel obcaecati nostrum iure laboriosam numquam autem. Eum?";
 
+$badword = $_GET['badword'];
+
+$censured = str_replace($badword, "***", $variabile);
 
 ?>
 
 
-<h1><?php echo $variabile ?></h1>
+<h1>
+    Testo:
+</h1>
 
-<h2> Lunghezza paragrafo <?php echo strlen($variabile)?></h2>
 
+<p>
+    <?php echo $variabile ?>
+</p>
+
+<h2> Lunghezza paragrafo: <?php echo strlen($variabile)?></h2>
+
+
+<hr>
+
+
+<h1>Testo Censurato</h1>
+
+<p>
+    <?php echo $censured ?>
+</p>
+
+<h2> Lunghezza paragrafo: <?php echo strlen($censured)?></h2>
